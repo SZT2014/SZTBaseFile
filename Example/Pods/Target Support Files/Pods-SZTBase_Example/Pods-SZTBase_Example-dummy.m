7467253857c151a0dd242b7ab66fce3018fa3dac
shuaizhitao <shuaizhitao@126.com>
2020-02-13 11:10:26 +0800

    修改specs中的远程库路径

---
+++ Example/Pods/Target Support Files/Pods-SZTBase_Example/Pods-SZTBase_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SZTBase_Example : NSObject
+@end
+@implementation PodsDummy_Pods_SZTBase_Example
+@end
---
+++ Example/Pods/Target Support Files/SZTBase/SZTBase-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SZTBase : NSObject
+@end
+@implementation PodsDummy_SZTBase
+@end
